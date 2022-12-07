785162ea13cfe980c2e0e1338224b8cbbe2d9c92
Tom Clifford <tomclifford@btinternet.com>
2022-12-07 14:57:50 +0000

    Day 4

---
+++ Day4/Day4Puzzle1.cs
@@ -0,0 +1,23 @@
+namespace Day4;
+
+class Day4Puzzle1 
+{
+    public static void output(string inputLocation)
+    {
+        var inputString = File.ReadAllText(inputLocation);
+        var assignments = inputString.Split("\n");
+        var assigmentsSplit = assignments.Select(x => x.Split(",").Select(y => y.Split("-")));
+        var solution = assigmentsSplit
+            .Where(x => 
+            (int.Parse(x.First().First()) <= int.Parse(x.Last().First()) 
+            && 
+            int.Parse(x.First().Last()) >= int.Parse(x.Last().Last()))
+            ||
+            (int.Parse(x.First().First()) >= int.Parse(x.Last().First())
+            &&
+            int.Parse(x.First().Last()) <= int.Parse(x.Last().Last()))
+            ).Count();
+        //var test3 = test.Where(x => x.Select(y => int.Parse(y.First()) > int.Parse(y.Last())));
+        Console.WriteLine($"Day4Puzzle1 solution: {solution}");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,4 +6,6 @@ Console.WriteLine("Hello, World!");
 //Day2.Day2Puzzle1.output("Day2/input.txt");
 //Day2.Day2Puzzle2.output("Day2/input.txt");
 //Day3.Day3Puzzle1.output("Day3/input.txt");
-Day3.Day3Puzzle2.output("Day3/input.txt");
+//Day3.Day3Puzzle2.output("Day3/input.txt");
+//Day4.Day4Puzzle1.output("Day4/input.txt");
+Day4.Day4Puzzle2.output("Day4/input.txt");
